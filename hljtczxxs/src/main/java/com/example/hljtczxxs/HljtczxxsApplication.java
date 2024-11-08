aab64953942d7cc8a80642ac5357544dc36dc6f7
thhyjyy <3942991003@qq.com>
2024-11-08 14:07:28 +0800

    Add files via upload

---
+++ hljtczxxs/src/main/java/com/example/hljtczxxs/HljtczxxsApplication.java
@@ -0,0 +1,13 @@
+package com.example.hljtczxxs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HljtczxxsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HljtczxxsApplication.class, args);
+    }
+
+}
---
+++ hljtczxxs/src/test/java/com/example/hljtczxxs/HljtczxxsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hljtczxxs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HljtczxxsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
